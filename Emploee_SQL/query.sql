61ff55b64c714e1a5ac6c77bf5baa2abcaf28229
btikeng <tikengomual@gmail.com>
2021-04-21 21:21:28 -0500

    changed

--- Emploee_SQL/query.sql
+++ Emploee_SQL/query.sql
@@ -16,7 +16,7 @@ FROM employees WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
 
 --3 List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name, 
-FROM departments,
+FROM departments
 JOIN dept_manager
 ON departments.dept_no = dept_manager.dept_no
 JOIN employees
